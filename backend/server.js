32fb7ed6e477e3f56a6aa1ec70d6413394236459
Orfanel-0205 <202211750@fit.edu.ph>
2025-08-14 00:02:23 +0800

    Add backend project files excluding environment config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5001;
+
 app.get("/", (req, res) => {
-    res.send("Hello from the server");
+    res.send("Hello from the server Clifford ");
 });
 
+console.log("My port", process.env.PORT);
+
 app.listen(5001, () => {
-    console.log("Server is running on PORT:5001");
+    console.log("Server is running on PORT:", PORT);
 });
